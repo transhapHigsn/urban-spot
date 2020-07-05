6c758bf60d32d74efce0f335bcfc3a2cad190ce0
transhapHigsn <fet.prashantsingh@gmail.com>
2020-07-06 00:01:39 +0530

    changes in worker  module

--- worker/main.tf
+++ worker/main.tf
@@ -3,16 +3,39 @@ provider "aws" {
   profile = "default"
 }
 
+data "aws_instance" "master" {
+
+  filter {
+    name   = "image-id"
+    values = [var.ami_id]
+  }
+
+  filter {
+    name   = "tag:Name"
+    values = ["master-1"]
+  }
+
+  filter {
+    name   = "tag:Terraform"
+    values = ["true"]
+  }
+
+  filter {
+    name   = "tag:KubernetesCluster"
+    values = [var.cluster_name]
+  }
+}
+
 resource "aws_instance" "worker-node-12" {
   ami                  = var.ami_id
   instance_type        = var.worker_instance_type
   subnet_id            = var.public_subnets[0]
-  key_name             = aws_key_pair.deployer.key_name
-  iam_instance_profile = aws_iam_instance_profile.k3s-master-role-profile.id
+  key_name             = data.aws_instance.master.key_name
+  iam_instance_profile = data.aws_instance.master.iam_instance_profile
 
   user_data = templatefile("data/agent-init.tmpl", {
-    master_ip       = aws_instance.master-node-12.public_ip,
-    master_local_ip = aws_instance.master-node-12.private_ip,
+    master_ip       = data.aws_instance.master.public_ip,
+    master_local_ip = data.aws_instance.master.private_ip,
     node_token      = var.k3s_token,
     cluster_name    = var.cluster_name,
     label           = "main-stream",
@@ -35,12 +58,12 @@ resource "aws_instance" "worker-node-11" {
   ami                  = var.ami_id
   instance_type        = var.worker_instance_type
   subnet_id            = var.public_subnets[1]
-  key_name             = aws_key_pair.deployer.key_name
-  iam_instance_profile = aws_iam_instance_profile.k3s-master-role-profile.id
+  key_name             = data.aws_instance.master.key_name
+  iam_instance_profile = data.aws_instance.master.iam_instance_profile
 
   user_data = templatefile("data/agent-init.tmpl", {
-    master_ip       = aws_instance.master-node-12.public_ip,
-    master_local_ip = aws_instance.master-node-12.private_ip,
+    master_ip       = data.aws_instance.master.public_ip,
+    master_local_ip = data.aws_instance.master.private_ip,
     node_token      = var.k3s_token,
     cluster_name    = var.cluster_name,
     label           = "main-stream",
--- worker/variables.tf
+++ worker/variables.tf
@@ -1,17 +1,12 @@
 # copy token from master node and paste here.
 variable "k3s_token" {
   type    = string
-  default = "K104bb90b7167be4dfef42ef09263e20a8ab006e64c8592d37322fce9c6f95cabf6::server:dqlitecluster123"
+  default = ""
 }
 
 variable "asg-enable-metrics" {
   type    = list(string)
   default = ["GroupDesiredCapacity", "GroupInServiceInstances", "GroupMaxSize", "GroupMinSize", "GroupPendingInstances", "GroupStandbyInstances", "GroupTerminatingInstances", "GroupTotalInstances"]
-}
-
-variable "deployer-public-key" {
-  type    = string
-  default = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCaiU+rxqvqD+OYiab58cRghIO8Pxd6E6XK63GKa+nKjKFaIfAq6q9QdYwZo42hDV0osz3u3tzww8hTczZ0gw9eMqqN9cLgeedu6nSMLInQY/YxsNTjcEB0eCld9iFksru0BkFXPz0ZmWR2bI0oU8qd1GxmRRC70OIb0ACVz48Hj3tuQJpAIn2xX9Ao08jZ735vXCXIX2arqxQ0UJOCkL9f2WEj/eHP6+00kz2FCgb/7dxXh624ivO0PK5P40vOqQnSJnrdwD5BhQ+6nudsGhTQAzdgHWSaCSNAVGA6uuIGylgYkQQHW8yOIzr7cgBW0rJoJDeNoFVOFy3Yjl6s3JFGJFL0FAIvMsDnSNiqVwOZjpWgyrDeY5SReE8xX+lPf/fNnaIyjfEaIGNDwPrTr9hafmeYecoOuX+4UMxPD1DDESF3jiXZWWxJVsXV84A5JOkVyDKtcMm7kuGqeIQbfRqChbbzzAgtgMg6uPPWy0foBMVH9RdpEK9NkkvPoU+e1v8= higsn@saitama"
 }
 
 variable "cluster_name" {
