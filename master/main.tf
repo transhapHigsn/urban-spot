074a8677c67a279799b43406eaabcf0776739eb7
transhapHigsn <fet.prashantsingh@gmail.com>
2020-07-06 21:14:47 +0530

    changes for creating master

--- master/main.tf
+++ master/main.tf
@@ -1,6 +1,6 @@
 provider "aws" {
-  region  = "us-east-2"
-  profile = "default"
+  region  = var.region
+  profile = var.profile
 }
 
 
@@ -13,16 +13,31 @@ data "aws_iam_instance_profile" "master_profile" {
   name = "k3s-master-role-profile"
 }
 
+data "aws_subnet" "public_subnet_1" {
+  cidr_block = var.public_subnet_cidr_blocks[0]
+}
+
+data "aws_subnet" "public_subnet_2" {
+  cidr_block = var.public_subnet_cidr_blocks[1]
+}
+
+data "aws_subnet" "public_subnet_3" {
+  cidr_block = var.public_subnet_cidr_blocks[2]
+}
+
 
 resource "aws_instance" "master-node-12" {
   ami                  = var.ami_id
   instance_type        = var.master_instance_type
-  subnet_id            = var.public_subnets[0]
+  subnet_id            = data.aws_subnet.public_subnet_1.id
   key_name             = aws_key_pair.deployer.key_name
-  iam_instance_profile = data.aws_iam_instance_profile.master_profile.id
+  iam_instance_profile = data.aws_iam_instance_profile.master_profile.name
   # vpc_security_group_ids = [module.bastion.private_instances_security_group]
 
-  user_data = templatefile("data/main-server-init.tmpl", { cluster_name = var.cluster_name })
+  user_data = templatefile("data/main-server-init.tmpl", {
+    cluster_name = var.cluster_name,
+    cluster_server_token = var.cluster_server_token
+  })
 
   tags = {
     Terraform                                   = "true"
@@ -37,22 +52,23 @@ resource "aws_instance" "master-node-12" {
 
 resource "null_resource" "echo_master_ip" {
   provisioner "local-exec" {
-    command = "sleep 120s && echo ${aws_instance.master-node-12.public_ip}"
+    command = "sleep 210s && echo ${aws_instance.master-node-12.public_ip}"
   }
 }
 
 resource "aws_instance" "master-node-11" {
   ami                  = var.ami_id
   instance_type        = var.master_instance_type
-  subnet_id            = var.public_subnets[1]
+  subnet_id            = data.aws_subnet.public_subnet_2.id
   key_name             = aws_key_pair.deployer.key_name
-  iam_instance_profile = data.aws_iam_instance_profile.master_profile.id
+  iam_instance_profile = data.aws_iam_instance_profile.master_profile.name
   # vpc_security_group_ids = [module.bastion.private_instances_security_group]
 
   user_data = templatefile("data/server-init.tmpl", {
     cluster_name   = var.cluster_name,
     DOCKER_IP      = aws_instance.master-node-12.private_ip,
-    MAIN_PUBLIC_IP = aws_instance.master-node-12.public_ip
+    MAIN_PUBLIC_IP = aws_instance.master-node-12.public_ip,
+    cluster_server_token = var.cluster_server_token
   })
 
   tags = {
@@ -64,24 +80,23 @@ resource "aws_instance" "master-node-11" {
     "kubernetes.io/cluster/${var.cluster_name}" = "owned"
   }
 
-  depends_on = [
-    aws_instance.master-node-12.id,
-  ]
+  depends_on = [null_resource.echo_master_ip]
 
 }
 
 resource "aws_instance" "master-node-10" {
-  ami                  = "ami-026dea5602e368e96"
-  instance_type        = "t3a.large"
-  subnet_id            = var.public_subnets[2]
+  ami                  = var.ami_id
+  instance_type        = var.master_instance_type
+  subnet_id            = data.aws_subnet.public_subnet_3.id
   key_name             = aws_key_pair.deployer.key_name
-  iam_instance_profile = data.aws_iam_instance_profile.master_profile.id
+  iam_instance_profile = data.aws_iam_instance_profile.master_profile.name
   # vpc_security_group_ids = [module.bastion.private_instances_security_group]
 
   user_data = templatefile("data/server-init.tmpl", {
     cluster_name   = var.cluster_name,
     DOCKER_IP      = aws_instance.master-node-12.private_ip,
-    MAIN_PUBLIC_IP = aws_instance.master-node-12.public_ip
+    MAIN_PUBLIC_IP = aws_instance.master-node-12.public_ip,
+    cluster_server_token = var.cluster_server_token
   })
 
   tags = {
@@ -93,9 +108,7 @@ resource "aws_instance" "master-node-10" {
     "kubernetes.io/cluster/${var.cluster_name}" = "owned"
   }
 
-  depends_on = [
-    aws_instance.master-node-12.id,
-  ]
+  depends_on = [null_resource.echo_master_ip]
 
 }
 
@@ -103,14 +116,14 @@ resource "aws_instance" "master-node-10" {
 
 resource "null_resource" "copy-kubeconfig" {
   provisioner "local-exec" {
-    command = "scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ubuntu@${aws_instance.master-node-12.public_ip}:/etc/rancher/k3s/k3s.yaml ./k3s-kubeconfig.yaml"
+    command = "scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ec2-user@${aws_instance.master-node-12.public_ip}:/etc/rancher/k3s/k3s.yaml ./k3s-kubeconfig.yaml"
   }
   depends_on = [null_resource.echo_master_ip]
 }
 
 resource "null_resource" "copy-nodetoken" {
   provisioner "local-exec" {
-    command = "scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ubuntu@${aws_instance.master-node-12.public_ip}:/home/ubuntu/node-token ./node-token"
+    command = "scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ec2-user@${aws_instance.master-node-12.public_ip}:/home/ec2-user/node-token ./node-token"
   }
   depends_on = [null_resource.echo_master_ip]
 }
