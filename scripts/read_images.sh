64b3058f4aa2a290ba50cdd90b61775adf394489
transhapHigsn <fet.prashantsingh@gmail.com>
2020-12-27 20:38:53 +0530

    chore: docker registry and read images setup
    Signed-off-by: transhapHigsn <fet.prashantsingh@gmail.com>

---
+++ scripts/read_images.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# update this according to cloud provider. this is for upcloud.
+PRIV_IP=$(curl http://169.254.169.254/metadata/v1/network/interfaces/2/ip_addresses/1/address)
+
+while read LINE
+do
+    echo "$LINE"
+    
+    org_name=$(echo "$LINE" | cut -d "/" -f 2)
+    name=$(echo "$LINE" | cut -d "/" -f 3)
+    final_image_name=$(echo $org_name/$name)
+    echo "$final_image_name"
+
+    local_image_name=$(echo $PRIV_IP:5000/$org_name/$name)
+
+    docker pull $LINE
+    docker tag $final_image_name $local_image_name
+    docker push $local_image_name
+
+done < $(pwd)/images.txt
+
+echo "Yeah! All required images are pulled into private registry"
---
+++ scripts/setup_registry_ubuntu.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+sudo apt-get -y update
+sudo apt-get -y install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+sudo apt-get -y update
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io
+
+# update this according to cloud provider, this is for upcloud.
+export PRIV_IP=$(curl http://169.254.169.254/metadata/v1/network/interfaces/2/ip_addresses/1/address)
+
+cat << EOF >> /etc/docker/daemon.json
+{"insecure-registries": ["$PRIV_IP:5000"]}
+EOF
+
+sudo service docker restart
+
+sudo docker run -d -p 5000:5000 --restart=always -e REGISTRY_STORAGE_DELETE_ENABLED=true --name registry registry:2
